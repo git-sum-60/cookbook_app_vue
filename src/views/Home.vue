ed65851837cd9b5134ce2d5d0e39c835ecffedf2
brian <riceb53@gmail.com>
2020-04-06 21:41:34 -0400

    add hardcoded new/create

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <h1>{{ name }}</h1>
+    <button v-on:click="addRecipe()">Add a new recipe</button>
     <!-- <h1>{{ recipes }}</h1> -->
     <!-- recipes.each do |recipe| -->
     <div v-bind:key="recipe.id" v-for="recipe in recipes">
@@ -38,6 +39,27 @@ export default {
       this.recipes = response.data;
     })
   },
-  methods: {}
+  methods: {
+    addRecipe: function() {
+      console.log('adding the recipe')
+      // get some data
+      var params = {
+        input_title: "Grapes",
+        input_ingredients: "time + vines",
+        input_directions: "trim the vines?",
+        input_prep_time: 20,
+        input_image_url: "https://www.thepacker.com/sites/default/files/Grapes_Red_web_.jpg"
+      }
+
+      // send it to rails
+      axios.post("/api/recipes", params).then(response => {
+        console.log(response.data);
+        this.recipes.push(response.data)
+      })
+
+
+      // add the new data to the html page
+    }
+  }
 };
 </script>
